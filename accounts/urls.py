e5715cf8dd67550e3a2dce2065d5a6d4e94439ac
johnj974 <jjdug@hotmail.com>
2020-04-17 13:24:22 +0000

    added url paths

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import home, products, customers
+
+
+urlpatterns = [
+    path("", home),
+    path("products/", products),
+    path("customers/", customers),
+]
