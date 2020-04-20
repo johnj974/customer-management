55bd62b3cfd6e374b0c603e384ef28b0ee02feb9
johnj974 <jjdug@hotmail.com>
2020-04-20 08:30:16 +0000

    added tag model to database

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,12 @@ class Customer(models.Model):
     phone = models.CharField(max_length=50)
     email = models.CharField(max_length=50, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    def __str__(self):
+        return self.name
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, null=True)
     def __str__(self):
         return self.name
 
@@ -19,11 +24,12 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.FloatField()
     category = models.CharField(max_length=50, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-
+    tags = models.ManyToManyField(Tag)
     def __str__(self):
         return self.name
+
 
 
 class Order(models.Model):
@@ -32,7 +38,8 @@ class Order(models.Model):
         ("Out for delivery", "Out for delivery"),
         ("Delivered", "Delivered"),
     )
-    # customer =
-    # product =
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=50, null=True, choices = STATUS)
+   
