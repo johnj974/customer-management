c83e8f67632b66a863c992e6fe5b283907e0137d
johnj974 <jjdug@hotmail.com>
2020-04-28 15:00:54 +0000

    adding reset password function/ added env variables

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import home, products, customers, createOrder, updateOrder, deleteOrder, loginpage, register, logoutUser, userPage, accountSettings
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", home, name="home"),
@@ -13,5 +13,9 @@ urlpatterns = [
     path("logout/", logoutUser, name="logout"),
     path("register/", register, name="register"),
     path("user/", userPage, name="user_page"),
-    path("account/", accountSettings, name="account")
+    path("account/", accountSettings, name="account"),
+    path("reset_password/", auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("reset/<uidb64>/<token>", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
--- amanagement/settings.py
+++ amanagement/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from os import path
+if path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    'accounts.apps.AccountsConfig',
     'django_filters',
 ]
 
@@ -128,3 +131,5 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
+
+
